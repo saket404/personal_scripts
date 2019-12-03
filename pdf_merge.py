97a16fa70cc4db3e0a8ed2199eeae52878e4ca6a
Saket Khandelwal <saketdecade17@gmail.com>
2019-12-03 13:01:43 +0700

    Added requirements and pdf merge script

---
+++ pdf_merge.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfFileMerger
+import sys
+
+if len(sys.argv) > 1:
+    pdfs = sys.argv[1:]
+    merger = PdfFileMerger()
+    
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    merger.write("result.pdf")
+    merger.close()
